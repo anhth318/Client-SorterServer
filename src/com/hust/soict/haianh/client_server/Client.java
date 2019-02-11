35853d1bb37b59b34c33e0119961f3acd2c9c44a
HaiAnhTRAN <anhth@soict.hust.edu.vn>
2019-02-11 15:26:35 +0700

    remove unused library

--- src/com/hust/soict/haianh/client_server/Client.java
+++ src/com/hust/soict/haianh/client_server/Client.java
@@ -1,7 +1,6 @@
 package com.hust.soict.haianh.client_server;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
